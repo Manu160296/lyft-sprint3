f0fcb8ab13d68f47aeab3d5dd2b8ffe37c170d42
Manu160296 <fmanuela499@gmail.com>
2017-12-30 09:14:58 -0500

    redireccionando vistas : index.html , home.html y sign-up-html

---
+++ js/home.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+//obtenemos el botón sign-up para redireccinar la página:
+var signUp = $('.sign-up-js');
+signUp.on('click', function () {
+    window.location.href = 'sign-up.html';
+})
+});
--- js/sign-up.js
+++ js/sign-up.js
@@ -1,1 +1,18 @@
-$("#phone").intlTelInput();
+$(document).ready(function () {
+
+    $("#phone").intlTelInput();
+
+//obtenemos el botón que redireccionará la vista anterior:
+var previousView = $('.previous-view-js')
+//asociamos el evento click:
+previousView.on('click' , function () {
+    window.location.href = 'home.html';
+
+});
+
+//obtenemos el boton 'next-js' para redireccionar la página
+var buttonNext = $('.next-js');
+console.log(buttonNext);
+
+
+});
