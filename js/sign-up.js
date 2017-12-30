a7dcfa5455cfb8a9d03f57662ae04ac2be0b3860
Manu160296 <fmanuela499@gmail.com>
2017-12-30 12:02:23 -0500

    funcionalidad sign-up.js

--- js/sign-up.js
+++ js/sign-up.js
@@ -10,9 +10,39 @@ previousView.on('click' , function () {
 
 });
 
+//obtenemos el input para validar la cantidad de digitos ingresada:
+
+var inputNumber = $('.input-js');
 //obtenemos el boton 'next-js' para redireccionar la página
 var buttonNext = $('.next-js');
-console.log(buttonNext);
+
+// asociamos el evento input:
+inputNumber.on('input' , function () {
+  // expresión regular que valida entrada de numeros:
+  //  console.log($(this).val())
+    var regExp = /^([0-9])*$/;
+//booleano para habilitar o deshabilitar boton:
+ var validateNumber = false ;
+// console.log(regExp.test($(this).val()))
+   if( regExp.test($(this).val()) && $(this).val().length === 10 ) {
+        console.log('habilitado')
+        validateNumber = true ;
+        buttonNext.removeClass('disabled');      
+    }  else {
+        buttonNext.addClass('disabled');
+    }
+
+});
+
+//ascociamos el evento  click a buttonNext :
+   buttonNext.on('click' , function () {
+    var randomCode = Math.trunc(Math.random() * (999 - 100) + 100)
+       alert('Tu código ' + 'LAB-'+ randomCode)
+    window.location.href = 'verify-phone-number.html';
+   });
+
+
+//console.log(buttonNext);
 
 
 });
